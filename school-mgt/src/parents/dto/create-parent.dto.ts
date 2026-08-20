b464ed68f83af96475919ed19ea746f466598266
Oluwatobi843 <alaooluwatobi97@gmail.com>
2026-08-20 09:35:20 +0100

    Add parents module with DTOs and tests
    Introduce ParentsModule with  ParentsController and ParentsService, plus unit tests for both.

---
+++ school-mgt/src/parents/dto/create-parent.dto.ts
@@ -0,0 +1,48 @@
+
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateParentDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(100)
+  firstName!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(100)
+  lastName!: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @MaxLength(255)
+  email!: string;
+
+  @IsPhoneNumber('NG')
+  @IsNotEmpty()
+  phoneNumber!: string;
+
+  @IsOptional()
+  @IsPhoneNumber('NG')
+  alternatePhoneNumber?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(500)
+  address?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(150)
+  occupation?: string;
+}
+
