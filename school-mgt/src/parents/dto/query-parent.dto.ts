7af2c91caec3a687185bda5d966bfe140d8d76c9
Oluwatobi843 <alaooluwatobi97@gmail.com>
2026-08-20 09:44:00 +0100

    Add QueryParentDto for parent queries
    Introduce QueryParentDto

---
+++ school-mgt/src/parents/dto/query-parent.dto.ts
@@ -0,0 +1,31 @@
+
+import {
+  IsBooleanString,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class QueryParentDto {
+  @IsOptional()
+  @Type(() => Number)
+  @Min(1)
+  page?: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @Min(1)
+  @Max(100)
+  limit?: number = 10;
+
+  @IsOptional()
+  @IsString()
+  search?: string;
+
+  @IsOptional()
+  @IsBooleanString()
+  isActive?: string;
+}
+
