1633b5ea55e387128b4a1a60f25c94ea8c16d175
Oluwatobi843 <alaooluwatobi97@gmail.com>
2026-08-22 22:16:47 +0100

    Add parent-student link DTO
    Add a DTO for linking a parent to a student with validation for the student ID, relationship type.

---
+++ school-mgt/src/parents/dto/link-parent-student.dto.ts
@@ -0,0 +1,27 @@
+
+import {
+  IsBoolean,
+  IsEnum,
+  IsInt,
+  IsOptional,
+  Min,
+} from 'class-validator';
+
+import { ParentRelationship } from '../entities/parent-student.entity';
+
+export class LinkParentStudentDto {
+  @IsInt()
+  @Min(1)
+  studentId!: number;
+
+  @IsEnum(ParentRelationship)
+  relationship!: ParentRelationship;
+
+  @IsOptional()
+  @IsBoolean()
+  isPrimaryContact?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  isEmergencyContact?: boolean;
+}
